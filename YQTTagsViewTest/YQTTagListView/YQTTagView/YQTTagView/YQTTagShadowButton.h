ea2739b36401ebc53a1ec50769f4f7ec2a560b80
hengyangKing <hengyangKing.yeah.net>
2018-01-30 15:23:13 +0800

    add shadow view and fix size

---
+++ YQTTagsViewTest/YQTTagListView/YQTTagView/YQTTagView/YQTTagShadowButton.h
@@ -0,0 +1,13 @@
+//
+//  YQTTagShadowButton.h
+//  YQTTagsViewTest
+//
+//  Created by J on 2018/1/30.
+//  Copyright © 2018年 J. All rights reserved.
+//
+
+#import "YQTTagContentButton.h"
+
+@interface YQTTagShadowButton : YQTTagContentButton
+
+@end
---
+++ YQTTagsViewTest/YQTTagListView/YQTTagView/YQTTagView/YQTTagShadowButton.m
@@ -0,0 +1,21 @@
+//
+//  YQTTagShadowButton.m
+//  YQTTagsViewTest
+//
+//  Created by J on 2018/1/30.
+//  Copyright © 2018年 J. All rights reserved.
+//
+
+#import "YQTTagShadowButton.h"
+
+@implementation YQTTagShadowButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
